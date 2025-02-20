0cf3974744732b8282616f09bbc8cc9e67a0aaa3
Elza 🤠 <96082339+ElzaJung@users.noreply.github.com>
2025-02-20 17:31:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def translate():
     return jsonify({"translated_text": translated_text})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Flask should run on 8080
+    app.run(host='0.0.0.0', port=port)
