8b12396329c8daaa7feb122fcad5634f9834fba9
Elza 🤠 <96082339+ElzaJung@users.noreply.github.com>
2025-02-20 15:25:26 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-libretranslate --host 0.0.0.0 --port 5000
+# Use PORT from environment variable, default to 5000 if not set.
+PORT=${PORT:-5000}
+echo "Starting LibreTranslate on port ${PORT}"
+./venv/bin/libretranslate --host 0.0.0.0 --port ${PORT}
