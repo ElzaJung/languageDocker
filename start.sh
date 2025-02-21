def3d2ba57a6834f8745b7a980b0271409902780
Elza 🤠 <96082339+ElzaJung@users.noreply.github.com>
2025-02-21 14:28:32 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 echo "Starting LibreTranslate on port: ${PORT}"
 
 # Replace the shell with the actual LibreTranslate process:
-exec ./venv/bin/libretranslate --port "${PORT}"
+exec ./venv/bin/libretranslate --port "${PORT}" --load-only en,es,ko,zh
